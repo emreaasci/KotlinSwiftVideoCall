aa0c1d15aef7e2607bbccb47014b667f831860f8
emreasci <emreaasci@gmail.com>
2024-12-16 10:46:09 +0300

    Initial Commit

---
+++ KotlinSwiftVideoCall/KotlinSwiftVideoCallApp.swift
@@ -0,0 +1,17 @@
+//
+//  KotlinSwiftVideoCallApp.swift
+//  KotlinSwiftVideoCall
+//
+//  Created by Emre Aşcı on 16.12.2024.
+//
+
+import SwiftUI
+
+@main
+struct KotlinSwiftVideoCallApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ KotlinSwiftVideoCallTests/KotlinSwiftVideoCallTests.swift
@@ -0,0 +1,17 @@
+//
+//  KotlinSwiftVideoCallTests.swift
+//  KotlinSwiftVideoCallTests
+//
+//  Created by Emre Aşcı on 16.12.2024.
+//
+
+import Testing
+@testable import KotlinSwiftVideoCall
+
+struct KotlinSwiftVideoCallTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
